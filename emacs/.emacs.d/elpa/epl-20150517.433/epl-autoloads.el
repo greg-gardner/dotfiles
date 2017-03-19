28d8e12e56c613f2fa0ea92a4a1daac4d6596590
theMainframer <mainframer.0@gmail>
2017-03-19 18:19:43 -0400

    Removed unnessessary files from source control.

--- emacs/.emacs.d/elpa/epl-20150517.433/epl-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; epl-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("epl.el") (21930 59546 240634 88000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; epl-autoloads.el ends here
