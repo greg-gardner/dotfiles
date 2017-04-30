dbf21184fab2406a6454be5bee43b1136f189c27
greg-gardner <gardner.r.greg@gmail.com>
2017-04-30 08:27:54 -0400

    Added shortcut for toggling line comment'

--- emacs/.emacs.d/shortcuts.el
+++ emacs/.emacs.d/shortcuts.el
@@ -1,7 +1,21 @@
 ;;;;;;;;;;;; macros.el
 ;;;;;;;;;;;; Store Greg's macros.
 
-;; switch buffers
+;;;;;;;;;;;;;;;;;; KEY BINDINGS ;;;;;;;;;;;;;;;;;;;;
 (global-set-key (kbd "C-<left>")  'previous-multiframe-window)
 (global-set-key (kbd "C-<right>") 'next-multiframe-window)
+(global-set-key (kbd "C-\\")      'toggle-comment-on-line)
 
+
+
+
+
+;;;;;;;;;;;;;;; FUNCTIONS DEFINITIONS ;;;;;;;;;;;;;;;;;;;
+
+;; Comment line
+(defun toggle-comment-on-line ()
+  "comment or uncomment current line"
+  (interactive)
+  (comment-or-uncomment-region (line-beginning-position) (line-end-position)))
+
+ 
